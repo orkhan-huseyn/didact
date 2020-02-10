b6aa6378d50e56383d7fdf63b4ae66dd7521ffe6
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2020-02-10 09:42:54 +0400

    initial commit

---
+++ readme.md
@@ -0,0 +1,4 @@
+# Minimal clone of React written for learning purposes
+
+Didact is clone of React written by [Roddigo Pombo](https://pomb.us/)
+You can find tutorial link [here](https://pomb.us/build-your-own-react/).
---
+++ sample.js
@@ -0,0 +1,14 @@
+function Counter() {
+  const [state, setState] = Didact.useState(1);
+  return Didact.createElement(
+    "h1",
+    { onClick: () => setState(c => c + 1) },
+    "Count: ",
+    state
+  );
+}
+
+const element = Didact.createElement(Counter);
+
+const container = document.getElementById("root");
+Didact.render(element, container);
